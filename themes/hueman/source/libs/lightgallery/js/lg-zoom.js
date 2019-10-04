4d05604da165e17ed707b26e01b0776a3584f210
AlexeyDolya <alexeyjezzadolya@gmail.com>
2019-10-04 17:33:15 +0300

    remove min css connection

--- themes/hueman/source/libs/lightgallery/js/lg-zoom.js
+++ themes/hueman/source/libs/lightgallery/js/lg-zoom.js
@@ -1,8 +1,8 @@
 /**!
  * lg-zoom.js | 0.0.2 | August 4th 2016
  * http://sachinchoolur.github.io/lg-zoom.js
- * Copyright (c) 2016 Sachin N; 
- * @license Apache 2.0 
+ * Copyright (c) 2016 Sachin N;
+ * @license Apache 2.0
  */(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.LgZoom = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 (function (global, factory) {
     if (typeof define === "function" && define.amd) {
@@ -254,7 +254,7 @@
         utils.removeClass(this.core.outer, 'lg-zoomed');
         for (var i = 0; i < this.core.___slide.length; i++) {
             if (this.core.___slide[i].querySelector('.lg-img-wrap')) {
-                this.core.___slide[i].querySelector('.lg-img-wrap').removeAttribute('style');
+                this.core.___slide[i].querySelector('.lg-img-wrap').removeAttribute('style.styl.css');
                 this.core.___slide[i].querySelector('.lg-img-wrap').removeAttribute('data-x');
                 this.core.___slide[i].querySelector('.lg-img-wrap').removeAttribute('data-y');
             }
@@ -262,7 +262,7 @@
 
         for (var j = 0; j < this.core.___slide.length; j++) {
             if (this.core.___slide[j].querySelector('.lg-image')) {
-                this.core.___slide[j].querySelector('.lg-image').removeAttribute('style');
+                this.core.___slide[j].querySelector('.lg-image').removeAttribute('style.styl.css');
                 this.core.___slide[j].querySelector('.lg-image').removeAttribute('data-scale');
             }
         }
